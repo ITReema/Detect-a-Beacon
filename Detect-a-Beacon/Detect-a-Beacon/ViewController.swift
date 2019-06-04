7ff52f2cfc0d3395b345ee0f4743e8c26f547c1c
Reema <it.reema7@gmail.com>
2019-06-04 03:33:32 +0300

    Add files via upload

---
+++ Detect-a-Beacon/Detect-a-Beacon/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Detect-a-Beacon
+//
+//  Created by mac_os on 01/10/1440 AH.
+//  Copyright © 1440 mac_os. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet var distanceReading: UILabel!
+    
+    var locationManager: CLLocationManager?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+        
+        view.backgroundColor = .gray
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    // do stuff
+                }
+            }
+        }
+    }
+
+
+}
+
